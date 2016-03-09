8ec7a5b3ec271389579b641d0d75b12c99d47495
Moritz Gelb <Moritz.Gelb@kit.edu>
2016-03-09 15:59:14 +0100

    rave::Propagator inherits now from AnalyticalPropagator.

--- src/RaveBase/RaveInterface/rave/Propagator.h
+++ src/RaveBase/RaveInterface/rave/Propagator.h
@@ -5,37 +5,42 @@
 #include <rave/Plane.h>
 #include <rave/Cylinder.h>
 #include <utility>
+#include <memory>
+
+#include "TrackingTools/GeomPropagators/interface/AnalyticalPropagator.h"
 
 namespace rave {
 
 /** 
- * @brief Guarantees that all necessary propagations can be performed. 
+ * @brief Guarantees that all necessary propagations can be performed. 3
  * @ingroup Rave
  *
  * Returns another rave::Track, albeit at a different position.
  */
 
-class  Propagator
+
+class  Propagator: public AnalyticalPropagator
 {
   public:
-    virtual Propagator * copy() const = 0;
+    virtual std::shared_ptr < Propagator > copy() const = 0;
     virtual rave::Track closestTo ( const rave::Track & orig,
                           const rave::Point3D &, bool transverse ) const = 0;
-    /**
-     *  propagate to plane, returing new track
-     *  and propagated distance 
-     */
+
+     // propagate to plane, returing new track
+     //  and propagated distance
+
     virtual std::pair < rave::Track, double > to ( const rave::Track & orig,
                           const ravesurf::Plane & ) const = 0;
-    /**
-     *  propagate to cylinder, returning new track
-     *  and propagated distance
-     */
+
+    //  propagate to cylinder, returning new track
+    //  and propagated distance
+
     virtual std::pair < rave::Track, double > to ( const rave::Track & orig,
                           const ravesurf::Cylinder & ) const = 0;
 
     virtual ~Propagator() {};
 };
+
 
 } // namespace rave
 
