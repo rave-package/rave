05853402355af3551e658ceafc08acfd14fa9088
Moritz Gelb <Moritz.Gelb@kit.edu>
2016-03-11 11:47:03 +0100

    Cleaned up rave::Vector6D class.

--- src/RaveBase/RaveInterface/rave/Vector6D.h
+++ src/RaveBase/RaveInterface/rave/Vector6D.h
@@ -1,64 +1,14 @@
 #ifndef _RAVEVECTOR6D_H_
 #define _RAVEVECTOR6D_H_
 
-//#include <rave/Vector3D.h>
-//#include <rave/Point3D.h>
-
-//#include "boost/operators.hpp"
-
 #include "TrackingTools/TrajectoryParametrization/interface/GlobalTrajectoryParameters.h"
 
 namespace rave
 {
+
 typedef GlobalTrajectoryParameters Vector6D;
+
 }
-/** \class RaveVector6D a 6-dimensional state
- *  Lengths are given in cm, momenta in GeV/c.
- */
 
-/**
-class  Vector6D :
-    boost::equality_comparable<Vector6D>
-{
-  public:
-
-    Vector6D();
-
-    Vector6D( float x, float y, float z,
-              float px, float py, float pz );
-
-    Vector6D( const Point3D & position,
-              const Vector3D & momentum );
-
-    virtual ~Vector6D() {};
-
-    float x() const;
-    float y() const;
-    float z() const;
-
-    float px() const;
-    float py() const;
-    float pz() const;
-
-    Vector3D momentum() const;
-    Point3D position() const;
-
-    void addMomentum ( const Vector3D & other );
-
-    virtual bool equal(const Vector6D & other) const;
-
-  private:
-    CheckedFloat thex;
-    CheckedFloat they;
-    CheckedFloat thez;
-    CheckedFloat thepx;
-    CheckedFloat thepy;
-    CheckedFloat thepz;
-};
-
- bool operator==(const Vector6D & lhs, const Vector6D & rhs);
-
-} // namespace rave
-**/
 #endif
 
